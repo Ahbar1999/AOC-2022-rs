4fabc8a9a7abd34861e097f851fd43fc33893aa3
Ahbar <ahbar1234@gmail.com>
2023-05-20 20:41:14 +0530

    code for day 2 added

--- runnable/src/days/day_2.rs
+++ runnable/src/days/day_2.rs
@@ -1,15 +1,84 @@
 use crate::*;
 
+// game results and moves with there respective scores
+enum GameResult {
+    Win = 6,
+    Draw = 3,
+    Loss = 0,
+}
+
+enum Move {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+struct Game {
+     your_move: Move,
+     opponents_move: Move,
+     verdict: Option<(GameResult,u32)>,
+}
+
+impl Game {
+
+    fn new(moves: &Vec<&str>) -> Self {
+        // your_move: Move, opponents_move: Move
+        let opponents_move = match moves[0] {
+            "A" => {Move::Rock},
+            "B" => {Move::Paper},
+            _ => {Move::Scissors},
+        };
+
+        let your_move = match moves[1] {
+            "X" => {Move::Rock},    
+            "Y" => {Move::Paper},   
+            _ => {Move::Scissors},
+        };
+
+        let mut obj = Game{your_move, opponents_move, verdict: None};
+        obj.verdict = Some(obj.get_verdict());
+        
+        return obj;
+    }
+
+    fn get_verdict(&self) -> (GameResult, u32) {
+        match (&self.your_move, &self.opponents_move) {
+            (Move::Rock, Move::Paper) => {(GameResult::Loss, Move::Rock as u32 + GameResult::Loss as u32)},
+            (Move::Rock, Move::Scissors) => {(GameResult::Win, 1 + 3)},
+            (Move::Paper, Move::Scissors) => {(GameResult::Loss, 1 + 3)},
+            (Move::Paper, Move::Rock) => {(GameResult::Draw, 1 + 3)},
+            (Move::Scissors, Move::Paper) => {(GameResult::Draw, 1 + 3)},
+            (Move::Scissors, Move::Rock) => {(GameResult::Draw, 1 + 3)},
+            (Move::Rock, Move::Rock) => { (GameResult::Draw, 1 + 3) },
+            (Move::Paper, Move::Paper) => { (GameResult::Draw, 2 + 3) },
+            (Move::Scissors, Move::Scissors) => { (GameResult::Draw, 3 + 3) },
+        };
+
+        todo!("get verdict: result + score");
+    }
+
+}
+
 pub fn solve(test_path: &str) -> std::io::Result<()> {
     let file = File::open(&test_path)?; 
     
     let file_reader = BufReader::new(file); 
     let lines = file_reader.lines(); 
-   
-    // solution logic
+    let mut running_score = 0;
 
-    
-    println!("Answer for day_2's problem: {:?}", ); 
+    // solution logic
+    for line in lines {
+        // if it is a valid line
+        if let Ok(contents) = line {
+            // moves[0] => opponent's move, moves[1] => your move 
+            let moves: Vec<&str> = contents.split_whitespace().collect();
+            let game = Game::new(&moves);
+            // println!("{:}", game.get_verdict());
+            running_score += game.get_verdict().1;
+        }
+
+    }  
+    println!("Answer for day_2's problem: {:?}", running_score); 
     
     Ok(())
 }
--- runnable/src/main.rs
+++ runnable/src/main.rs
@@ -4,79 +4,24 @@ use std::io::BufReader;
 
 // use std::cmp;
 use std::env;
-use crate::days::day_1;
+use crate::days::day_2;
 
 mod days;
-/*
-mod day_1 {
-    use crate::*;
-    
-    fn update_max_calories(max_calories: &mut [u32], curr_calories: u32) {
-        for i in 0..max_calories.len() {
-            // update max calories at each index
-            if max_calories[i] <= curr_calories {
-                if i + 1 <= 2 {     
-                    if i + 2 <= 2 {
-                        max_calories[i + 2] = max_calories[i + 1];
-                    } 
-                    max_calories[i + 1] = max_calories[i];
-                }
-                max_calories[i] = curr_calories;
-                break; 
-            }
-        } 
-    }
-
-    pub fn solve(test_path: &str) -> std::io::Result<()> {
-        let file = File::open(&test_path)?; 
-        
-        let file_reader = BufReader::new(file); 
-        let lines = file_reader.lines(); 
-        let mut max_calories = [0; 3];
-        let mut curr_calories: u32 = 0; 
-        
-        for line in lines {
-            if let Ok(contents) = line {
-                // println!("Read line {:}", contents); 
-                if contents == "" {
-                    update_max_calories(&mut max_calories, curr_calories); 
-                    // println!("{:?}", max_calories);
-                    
-                    // reset counter of calories
-                    curr_calories = 0;
-                } else {
-                    curr_calories += str::parse::<u32>(contents.as_str().trim()).unwrap();   
-                } 
-            }
-        }
-        // finish up remaining curr_calories 
-        update_max_calories(&mut max_calories, curr_calories); 
-        // println!("{:?}", max_calories);
-
-        println!("Answer for day_1's problem: {:?}", max_calories.into_iter().sum::<u32>()); 
-        
-        Ok(())
-    }
-}
-
-mod day_2 {
-
-}
-*/
 
 fn main() {
     // call day_x with test input
     // build the input file path 
-    let day = "day_1"; 
+    let days = vec!["day_1", "day_2"];
+    
+    let day = days[1];
     let mut file_path = env::current_dir().unwrap();
     file_path.push("..");
     file_path.push("test_files");
     file_path.push(day);
     file_path.push("input.txt");
-
     // println!("{:}", file_path.to_str().unwrap()); 
     
-    if let Ok(_) = day_1::solve(file_path.to_str().unwrap()) {
+    if let Ok(_) = day_2::solve(file_path.to_str().unwrap()) {
         println!("Successfully solved day 1's problem");
     } else {
         println!("Failed to solve day 1's problem");
