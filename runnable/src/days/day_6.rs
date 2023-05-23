d53ab27a1f012fd17f525b72b40a9eceba508f7a
Ahbar <ahbar1234@gmail.com>
2023-05-23 13:52:31 +0530

    day 6 solved

--- runnable/src/days/day_6.rs
+++ runnable/src/days/day_6.rs
@@ -2,6 +2,47 @@ use crate::*;
 use std::collections::HashMap;
 
 
+
+fn find_marker_at_n(contents: &str, n: usize) {
+    // solution logic         
+    let mut last_n: HashMap<char, u32> = HashMap::new();
+    let mut ans: usize = 0;
+     
+    // set start pointer to the start of the string 
+    let mut start: (usize, char) = contents.chars().enumerate().next().unwrap(); 
+    // println!("start: {:?}", start); 
+    
+    for (i, ch) in contents.chars().enumerate() {
+        if !last_n.contains_key(&ch) {
+            last_n.insert(ch, 0);  
+        }
+        
+        let count = last_n.get(&ch).unwrap();
+        last_n.insert(ch, count + 1);
+    
+        if i - start.0 + 1 > n {
+            // remove contents[start]
+            let count = &last_n[&start.1];
+            last_n.insert(start.1, count - 1);
+            if *last_n.get(&start.1).unwrap() == 0 as u32 {
+                last_n.remove(&start.1); 
+            }
+
+            // increment start pointer
+            start = contents.chars().enumerate().nth(start.0 + 1).unwrap();
+        }
+
+        // if count of distinct elements = 4
+        if last_n.len() == n {
+            ans = i + 1;
+            println!("Answer for day_6's problem: {:?}", ans); 
+            break; 
+        }
+        // println!("{:?} {:?}", start, (i, ch));
+    }
+}
+
+
 pub fn solve(test_path: &str) -> std::io::Result<()> {
     let file = File::open(&test_path)?; 
     
@@ -10,44 +51,12 @@ pub fn solve(test_path: &str) -> std::io::Result<()> {
 
     for line in lines {
         if let Ok(contents) = line { 
-            
-            // solution logic         
-            let mut last_four: HashMap<char, u32> = HashMap::new();
-            let mut ans: usize = 0;
-             
-            // set start pointer to the start of the string 
-            let mut start: (usize, char) = contents.chars().enumerate().next().unwrap(); 
-            // println!("start: {:?}", start); 
-            
-            for (i, ch) in contents.chars().enumerate() {
-                if !last_four.contains_key(&ch) {
-                    last_four.insert(ch, 0);  
-                }
-                
-                let n = last_four.get(&ch).unwrap();
-                last_four.insert(ch, n + 1);
-            
-                if i - start.0 + 1 > 4 {
-                    // remove contents[start]
-                    let count = &last_four[&start.1];
-                    last_four.insert(start.1, count - 1);
-                    if *last_four.get(&start.1).unwrap() == 0 as u32 {
-                        last_four.remove(&start.1); 
-                    }
-
-                    // increment start pointer
-                    start = contents.chars().enumerate().nth(start.0 + 1).unwrap();
-                }
-
-                // if count of distinct elements = 4
-                if last_four.len() == 4 {
-                    ans = i + 1;
-                    println!("Answer for day_6's problem: {:?}", ans); 
-                    break; 
-                }
-
-                // println!("{:?} {:?}", start, (i, ch));
-            }  
+            // first part 
+            find_marker_at_n(&contents, 4);
+            println!("Second part's answer: ");
+            // second part
+            find_marker_at_n(&contents, 14); 
+            println!("######################"); 
         }
     }
      
